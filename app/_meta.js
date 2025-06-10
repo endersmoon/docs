63b609d5663944a02dd0b27f31d3f5bbc338c4a1
Tushar Debnath <endersmoon@gmail.com>
2025-06-10 10:04:09 +0530

    changes

--- app/_meta.js
+++
@@ -1,7 +0,0 @@
-export default {
-  index: { title: 'Home', type: 'page' },
-  doc: { title: 'Guide', type: 'page' },
-  components: { title: 'Components', type: 'page' },
-  tokens: { title: 'Tokens', type: 'page' },
-  assets: { title: 'Assets', type: 'page' },
-};
